97efc260d91bac40a0d851b0381cbcf2bc84bf7c
Rafał <duran.rafal@gmail.com>
2020-06-12 19:23:51 +0200

    Fixed rhyme checking.

--- Are_Rhymes.py
+++ Are_Rhymes.py
@@ -15,65 +15,70 @@ def are_rhyming(line1,line2):
     s2 = len(words2)
     rhyme1 = words1[s1-1]
     rhyme2 = words2[s2-1]
-    for char in rhyme1:
-        if (char == 'u' or char == 'U'):
-            if (prev_i == False and prev_ae == False):
+    for word in words1:
+        for char in word:
+            if (char == 'u' or char == 'U'):
+                if (prev_i == False and prev_ae == False):
+                    line1_syllabe += 1
+                if (prev_ae == True):
+                    rem = line1_syllabe
+            elif (char == 'a' or char == 'e' or char == 'A' or char == 'E'):
+                if (prev_i == True):
+                    prev_ae = False
+                    prev_i = False
+                else:
+                    prev_ae = True
+                    line1_syllabe += 1
+            elif (char == 'i' or char == 'I'):
+                prev_i = True
                 line1_syllabe += 1
-            if (prev_ae == True):
-                rem = line1_syllabe
-        elif (char == 'a' or char == 'e' or char == 'A' or char == 'E'):
-            if (prev_i == True):
+            elif (char == 'o' or char == 'ą' or char == 'ę' or char == 'ó' or char == 'y'):
+                if (prev_i == True):
+                    prev_i = False
+                else:
+                    line1_syllabe += 1
+            elif (char == 'O' or char == 'Ą' or char == 'Ę' or char == 'Ó' or char == 'Y'):
+                line1_syllabe += 1
+            else:
+                prev_i = False
                 prev_ae = False
-                prev_i = False
-            else:
-                prev_ae = True
-                line1_syllabe += 1
-        elif (char == 'i' or char == 'I'):
-            prev_i = True
-            line1_syllabe += 1
-        elif (char == 'o' or char == 'ą' or char == 'ę' or char == 'ó' or char == 'y'):
-            if (prev_i == True):
-                prev_i = False
-            else:
-                line1_syllabe += 1
-        elif (char == 'O' or char == 'Ą' or char == 'Ę' or char == 'Ó' or char == 'Y'):
-            line1_syllabe += 1
-        else:
-            prev_i = False
-            prev_ae = False
-    if (rem == line1_syllabe):
-        line1_syllabe += 1
 
-    for char in rhyme2:
-        if (char == 'u' or char == 'U'):
-            if (prev_i == False and prev_ae == False):
+    for word in words2:
+        for char in word:
+            if (char == 'u' or char == 'U'):
+                if (prev_i == False and prev_ae == False):
+                    line2_syllabe += 1
+                if (prev_ae == True):
+                    rem = line2_syllabe
+            elif (char == 'a' or char == 'e' or char == 'A' or char == 'E'):
+                if (prev_i == True):
+                    prev_ae = False
+                    prev_i = False
+                else:
+                    prev_ae = True
+                    line2_syllabe += 1
+            elif (char == 'i' or char == 'I'):
+                prev_i = True
                 line2_syllabe += 1
-            if (prev_ae == True):
-                rem = line2_syllabe
-        elif (char == 'a' or char == 'e' or char == 'A' or char == 'E'):
-            if (prev_i == True):
+            elif (char == 'o' or char == 'ą' or char == 'ę' or char == 'ó' or char == 'y'):
+                if (prev_i == True):
+                    prev_i = False
+                else:
+                    line2_syllabe += 1
+            elif (char == 'O' or char == 'Ą' or char == 'Ę' or char == 'Ó' or char == 'Y'):
+                line2_syllabe += 1
+            else:
+                prev_i = False
                 prev_ae = False
-                prev_i = False
-            else:
-                prev_ae = True
-                line2_syllabe += 1
-        elif (char == 'i' or char == 'I'):
-            prev_i = True
-            line2_syllabe += 1
-        elif (char == 'o' or char == 'ą' or char == 'ę' or char == 'ó' or char == 'y'):
-            if (prev_i == True):
-                prev_i = False
-            else:
-                line2_syllabe += 1
-        elif (char == 'O' or char == 'Ą' or char == 'Ę' or char == 'Ó' or char == 'Y'):
-            line2_syllabe += 1
-        else:
-            prev_i = False
-            prev_ae = False
-    if (rem == line2_syllabe):
-        line2_syllabe += 1
     isRhyme = True
-    if (line1_syllabe == line2_syllabe):
+    diff = abs(line1_syllabe - line2_syllabe)
+    len1 = len(rhyme1)
+    len2 = len(rhyme2)
+    if (not(97<=ord(rhyme1[len1 - 1])<=122)):
+        len1 -= 1
+    if (not(97<=ord(rhyme1[len2 - 1])<=122)):
+        len2 -= 1
+    if ((diff/max(line1_syllabe,line2_syllabe)) <= 0.3):
         for i in range(1,3):
             if (not(rhyme1[len(rhyme1)-i] == rhyme2[len(rhyme2)-i])):
                 isRhyme = False
@@ -85,4 +90,4 @@ def are_rhyming(line1,line2):
 
        
 
-print(are_rhyming("dyma","lama"))
+print(are_rhyming("Ty jesteś jak zdrowie.","w całej ozdobie,"))
